b3bd25e37b73e919cf43acf839586258012647f5
AngusG <maltawebdev@gmail.com>
2025-06-11 00:28:32 +0200

    Fix: logo and h1 fade in too quickly

--- src/HeroSection.tsx
+++ src/HeroSection.tsx
@@ -27,14 +27,16 @@ function HeroSection() {
       />
 
       {/* content after fade */}
-      {fade && (
-        <div className="relative z-10 flex h-full flex-col items-center justify-center text-center text-neutral-200">
-          <img src="/logo-autoxs.png" alt="AutoXS Malta" className="w-64 md:w-xl mb-6" />
-          <h1 className="text-4xl md:text-6xl font-heading font-bold leading-12">
-            Quality<br/>Car&nbsp;Care<br/>Products
-          </h1>
-        </div>
-      )}
+      <div
+        className={`relative z-10 flex h-full flex-col items-center justify-center text-center text-neutral-200 transition-opacity duration-[2000ms] delay-[1000ms] ${
+          fade ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        <img src="/logo-autoxs.png" alt="AutoXS Malta" className="w-64 md:w-xl mb-6" />
+        <h1 className="text-4xl md:text-6xl font-heading font-bold leading-12">
+          Quality<br/>Car&nbsp;Care<br/>Products
+        </h1>
+      </div>
     </section>
   );
 }
