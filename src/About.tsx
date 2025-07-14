c5cc078b93b9ec116cf257867a2369da1d4d1ddc
AngusG <maltawebdev@gmail.com>
2025-07-14 07:39:57 +0200

    Feat: add custom favicons with AutoXS logo

--- src/About.tsx
+++ src/About.tsx
@@ -8,9 +8,11 @@ export default function About() {
 // // About AutoXS Malta
 
 // Importers and distributors of car care and maintenance products and accessories, fuel treatments, oil additives and more. Our top brands are ArmorAll, STP and Nerta.
+
 // Armor All
 
 // Armor All® products help people preserve and protect the value and appearance of their cars. From interior products like Armor All® Protectant Wipes to exterior items like Armor All® Wash & Wax, Armor All® products go beyond simply washing to help maintain the appearance of a car for the long term.
+
 // STP
 
 // For over 60 years, and for more miles than anyone can count, STP® has been one of the most trusted names in automotive care. Car enthusiasts, “Do-it-Yourselfers” and professional racers rely on STP® oil additives, fuel additives and brake and power steering fluids to keep their vehicles running better longer. And they count on STP® automotive appearance products to keep their vehicles looking great, too. STP® , headquartered in Danbury, CT is part of the Armored AutoGroup.
