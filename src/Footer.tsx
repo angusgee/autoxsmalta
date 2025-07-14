cc2a77f2a84926d19230a9b78a73609c1d798bf1
AngusG <maltawebdev@gmail.com>
2025-07-14 07:24:59 +0200

    Feat: scaffold out three components

---
+++ src/Footer.tsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <div>Footer</div>
+  )
+}
+
+
+// AutoXS Malta
+// 28, Tal Millieri Apts
+// Hal-Ghaxaq, GXQ 1511
+// MALTA
+// Imports P: +356 99472443
+// Distribution P: +356 79273579
+// Sales P: +356 9985 9227
+// E: info@autoxsmalta.com
+// Facebook: AutoXS Malta
+
